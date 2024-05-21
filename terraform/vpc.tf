f7a24a67408b340c5ca73bd2aad8609c8ce7fe75
farid555 <farid555a@gmail.com>
2024-05-21 18:34:06 +0300

    Initial commit

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public-subnet" {
 }
 
 resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id       = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
