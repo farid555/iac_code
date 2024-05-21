d6b90f9ccfa6104a9681bcad5dfccfbadd9fc5d2
farid555 <farid555a@gmail.com>
2024-05-21 18:41:47 +0300

    terrform error

--- terraform/iam-policy.tf
+++ terraform/iam-policy.tf
@@ -1,5 +1,4 @@
 resource "aws_iam_role_policy_attachment" "iam-policy" {
-  role       = aws_iam_role.iam-role.name
-  # Just for testing purpose, don't try to give administrator access in production
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role        = aws_iam_role.iam-role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AdministratorAccess" # Just for testing purpose, don't try to give administrator access in production
 }
