9b22ee191b7a870115bfdb42267305632c93b914
farid555 <farid555a@gmail.com>
2024-05-24 02:27:22 +0300

    updated syntax

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,66 +1,66 @@
 variable "region" {
   description = "AWS region"
-  type        = string
+  type        =  string
   default     = "us-east-1"
 }
 
 variable "vpc-name" {
   description = "VPC Name for our Jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-vpc"
 }
 
 variable "igw-name" {
   description = "Internet Gate Way Name for our Jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-igw"
 }
 
 variable "subnet-name" {
   description = "Subnet Name for our Jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-subnet"
 }
 
 variable "rt-name" {
   description = "Route Table Name for our Jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-rt"
 }
 
 variable "sg-name" {
   description = "Security Group for our Jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-sg"
 }
 
 
 variable "iam-role" {
   description = "IAM Role for the Jumphost Server"
-  type        = string
+  type        =  string
   default     = "Jumphost-iam-role"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  type        = string
+  type        =  string
   default     = "ami-0c7217cdde317cfec" # Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
+  type        =  string
   default     = "t2.large"
 }
 
 variable "key_name" {
   description = "EC2 keypair"
-  type        = string
+  type        =  string
   default     = "terraform_newKey"
 }
 
 variable "instance_name" {
   description = "EC2 Instance name for the jumphost server"
-  type        = string
+  type        =  string
   default     = "Jumphost-server"
 }
